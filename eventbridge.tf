7e52a4c0c0d23a05ae4143404cd3222b21fc17d5
Wessel van Heerde <wvanheerde@schubergphilis.com>
2023-12-22 19:42:22 +0100

    fix: work with list of accounts to manage

--- eventbridge.tf
+++ eventbridge.tf
@@ -54,7 +54,7 @@ resource "aws_scheduler_schedule_group" "service_quotas_manager" {
 }
 
 resource "aws_scheduler_schedule" "sqm_collect_service_quotas" {
-  for_each = var.quotas_manager_configuration
+  for_each = toset(var.quotas_manager_configuration)
 
   name                         = "sqm-collect-service-quotas-${each.value.accountid}"
   group_name                   = aws_scheduler_schedule_group.service_quotas_manager.name
