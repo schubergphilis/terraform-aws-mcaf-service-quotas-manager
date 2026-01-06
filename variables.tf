72395e25b569a129a73df81c109f9da40e97455b
Shreya Vashisht <Shreya.Vashisht@devolksbank.nl>
2026-01-06 11:55:19 +0100

    testing tf run

--- variables.tf
+++ variables.tf
@@ -23,6 +23,7 @@ variable "execution_role" {
 variable "kms_key_arn" {
   description = "The ARN of the KMS key to use with the configuration S3 bucket and scheduler"
   type        = string
+  default     = null
 }
 
 variable "quotas_manager_configuration" {
